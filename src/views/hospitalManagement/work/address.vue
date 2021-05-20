d34af12797d48a99691417096650e658e53b0a27
xiongzihaoc <1254904706@qq.com>
2021-05-20 16:16:45 +0800

    111优化

--- src/views/hospitalManagement/work/address.vue
+++ src/views/hospitalManagement/work/address.vue
@@ -157,7 +157,7 @@
 <script>
 import EleTable from "@/components/Table";
 import { validatePhone } from "@/utils/index";
-import { list } from "@/api/admin/addressDoctor";
+import { list } from "@/api/admin/http_addressDoctor";
 export default {
   components: {
     EleTable,
