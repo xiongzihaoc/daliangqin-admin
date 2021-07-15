13ef6b26b8867ff3dd016adf8da864438341300a
xiongzihaoc <1254904706@qq.com>
2021-07-15 15:48:49 +0800

    优化

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ export function parseTime(time, cFormat) {
   if (arguments.length === 0 || !time) {
     return null
   }
-  const format = cFormat || '{y}-{m}-{d} {h}:{i}'
+  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
   let date
   if (typeof time === 'object') {
     date = time
