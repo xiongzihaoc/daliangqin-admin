d8f5dce6ff7b8308f89c44534cd891173fdfe3f3
柏沐桜 <2585201479@qq.com>
2021-09-29 18:16:21 +0800

    优化 监测统计 时间搜索

--- src/views/archivesManagement/monitoring/index.vue
+++ src/views/archivesManagement/monitoring/index.vue
@@ -22,6 +22,7 @@
             unlink-panels
             :default-value="new Date()"
             value-format="timestamp"
+            :default-time="['00:00:00', '23:59:59']"
             range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
@@ -121,19 +122,19 @@ export default {
             picker.$emit('pick', [start, end])
           }
         }, {
-          text: '昨天',
-          onClick(picker) {
-            const end = new Date()
-            const start = new Date()
-            start.setTime(start.getTime() - 3600 * 1000 * 24);
-            picker.$emit('pick', [start, end])
-          }
-        }, {
-          text: '一周前',
+          text: '最近一周',
           onClick(picker) {
             const end = new Date()
             const start = new Date()
             start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一个月',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
             picker.$emit('pick', [start, end])
           }
         }]
