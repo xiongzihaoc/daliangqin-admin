ad884dbc8d6cf72ab7e073902d1e8a2257680c95
xiongzihaoc <1254904706@qq.com>
2021-09-30 18:42:56 +0800

    优化

--- src/views/archivesManagement/monitoring/index.vue
+++ src/views/archivesManagement/monitoring/index.vue
@@ -2,20 +2,24 @@
   <div class="app-container">
     <!-- 搜索区域 -->
     <div class="search-box">
-      <el-form ref="searchFormRef" :model="searchForm" class="searchForm" :inline="true">
-        <el-form-item label="医院名称" align="left">
-          <el-select v-model="searchForm.hospitalId" size="small" filterable placeholder="请选择医院">
-            <el-option
-              v-for="item in hospitalList"
+      <el-form ref="searchFormRef"
+        :model="searchForm"
+        class="searchForm"
+        :inline="true">
+        <el-form-item label="医院名称"
+          align="left">
+          <el-select v-model="searchForm.hospitalId"
+            size="small"
+            filterable
+            placeholder="请选择医院">
+            <el-option v-for="item in hospitalList"
               :key="item.id"
               :label="item.name"
-              :value="item.id"
-            ></el-option>
+              :value="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="日期">
-          <el-date-picker
-            v-model="searchForm.superviseTime"
+          <el-date-picker v-model="searchForm.superviseTime"
             size="small"
             align="right"
             type="daterange"
@@ -27,71 +31,92 @@
             end-placeholder="结束日期"
             placeholder="选择日期"
             @change="changeMonitorTime"
-            :picker-options="pickerOptions"
-          ></el-date-picker>
+            :picker-options="pickerOptions"></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button @click="searchBtn" type="primary" size="small" icon="el-icon-search">搜索</el-button>
-          <el-button @click="searchReset" size="small" plain icon="el-icon-refresh">重置</el-button>
+          <el-button @click="searchBtn"
+            type="primary"
+            size="small"
+            icon="el-icon-search">搜索</el-button>
+          <el-button @click="searchReset"
+            size="small"
+            plain
+            icon="el-icon-refresh">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
     <!-- 表格区域 -->
-    <EleTable
-      :data="list"
+    <EleTable :data="list"
       :header="tableHeaderBig"
       :pageNum="pageNum"
       :pageSize="pageSize"
       :total="total"
       show-summary
       @handleSizeChange="handleSizeChange"
-      @handleCurrentChange="handleCurrentChange"
-    >
-      <el-table-column align="center" label="序号" type="index"></el-table-column>
-      <el-table-column align="center" label="医院名称" prop="hospitalName">
+      @handleCurrentChange="handleCurrentChange">
+      <el-table-column align="center"
+        label="序号"
+        type="index"></el-table-column>
+      <el-table-column align="center"
+        label="医院名称"
+        prop="hospitalName">
         <!-- <template slot-scope="scope">
           <span class="skipStyle" @click="skipHeart(scope.row)">{{ scope.row.hospitalName }}</span>
         </template>-->
       </el-table-column>
-      <el-table-column align="center" label="测量总人数" prop="measureTotalFrequency"></el-table-column>
-      <el-table-column align="center" label="测量总次数" prop="measureTotalAmount">
+      <el-table-column align="center"
+        label="测量总人数"
+        prop="measureTotalFrequency"></el-table-column>
+      <el-table-column align="center"
+        label="测量总次数"
+        prop="measureTotalAmount">
         <template slot-scope="scope">
-          <span
-            class="skipStyle"
-            @click="skipHeart(scope.row, 'people')"
-          >{{ scope.row.measureTotalAmount }}</span>
+          <span class="skipStyle"
+            @click="skipHeart(scope.row, 'people')">{{ scope.row.measureTotalAmount }}</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="公司已审核报告数" prop="companyAuditNumber">
+      <el-table-column align="center"
+        label="公司已审核报告数"
+        prop="companyAuditNumber">
         <template slot-scope="scope">
-          <span class="skipStyle" @click="skipHeart(scope.row, 'PLATFORM_COMPLETE_AUDIT')">
+          <span class="skipStyle"
+            @click="skipHeart(scope.row, 'PLATFORM_COMPLETE_AUDIT')">
             {{
               scope.row.companyAuditNumber
             }}
           </span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="医院已审核报告数" prop="hospitalAuditNumber">
+      <el-table-column align="center"
+        label="医院已审核报告数"
+        prop="hospitalAuditNumber">
         <template slot-scope="scope">
-          <span class="skipStyle" @click="skipHeart(scope.row, 'HOSPITAL_COMPLETE_AUDIT')">
+          <span class="skipStyle"
+            @click="skipHeart(scope.row, 'HOSPITAL_COMPLETE_AUDIT')">
             {{
               scope.row.hospitalAuditNumber
             }}
           </span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="公司待审核报告数" prop="companyWaitAuditNumber">
+      <el-table-column align="center"
+        label="公司待审核报告数"
+        prop="companyWaitAuditNumber">
         <template slot-scope="scope">
-          <span class="skipStyle" @click="skipHeart(scope.row, 'TO_AUDIT')">
+          <span class="skipStyle"
+            @click="skipHeart(scope.row, 'TO_AUDIT')">
             {{
               scope.row.companyWaitAuditNumber
             }}
           </span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="医院待审核报告数" prop="hospitalWaitAuditNumber">
+      <el-table-column align="center"
+        label="医院待审核报告数"
+        prop="hospitalWaitAuditNumber">
         <template slot-scope="scope">
-          <span class="skipStyle" @click="skipHeart(scope.row, 'TO_HOSPITAL_AUDIT')">
+          <span class="skipStyle"
+            @click="skipHeart(scope.row, 'TO_HOSPITAL_AUDIT')">
             {{
               scope.row.hospitalWaitAuditNumber
             }}
@@ -153,12 +178,6 @@ export default {
               picker.$emit('pick', [start, end])
             },
           },
-          {
-            text: '全部',
-            onClick(picker) {
-              picker.$emit('pick', '')
-            },
-          },
         ],
       },
       // 分页区域
@@ -197,7 +216,7 @@ export default {
         this.hospitalList = res.data.elements
       })
     },
-    getSummaries() { },
+    getSummaries() {},
     // 跳转心率检测
     skipHeart(val, state) {
       if (state != 'people') {
@@ -222,8 +241,10 @@ export default {
     },
     searchReset() {
       this.pageNum = 1
-      this.searchForm.startTime = new Date(new Date().toLocaleDateString()).getTime(),
-        this.searchForm.endTime = new Date().getTime(),
+      ;(this.searchForm.startTime = new Date(
+        new Date().toLocaleDateString()
+      ).getTime()),
+        (this.searchForm.endTime = new Date().getTime()),
         this.getList()
     },
     /**
