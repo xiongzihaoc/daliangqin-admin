4867e4ce0b698e1b44987cab1d67fdd949b26f71
xiongzihaoc <1254904706@qq.com>
2021-10-21 17:23:56 +0800

    优化

---
+++ src/views/archivesManagement/statistical/chart/monitoringNumber.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div id="monitoringNumber">
+      <Chart :cdata="cdata" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from '@/components/Echarts/chart';
+export default {
+  data() {
+    return {
+      cdata: {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'none',
+          },
+        },
+        title: {
+          text: '已监测数量统计图 Top15',
+          textStyle: {
+            color: '#000',
+            fontSize: 14,
+          },
+          padding: 20,
+        },
+        grid: {
+          left: '15%',
+        },
+        xAxis: {
+          axisLine: false,
+          type: 'category',
+          axisLabel: {
+            textStyle: {
+              color: '#ccc',
+            },
+          },
+          data: [
+            '夏家',
+            '杨墩',
+            '塘北',
+            '洛舍镇',
+            '戈亭',
+            '石泉',
+            '光辉',
+            '雷甸',
+            '城东',
+            '辖东港',
+          ],
+        },
+        yAxis: {
+          axisLine: false,
+          type: 'value',
+          axisLabel: {
+            textStyle: {
+              color: '#ccc',
+            },
+          },
+        },
+        series: [
+          {
+            data: [
+              8800, 10000, 10500, 11500, 12000, 13500, 14000, 14500, 15000,
+              16000,
+            ],
+            type: 'bar',
+            itemStyle: {
+              color: new this.$echarts.graphic.LinearGradient(1, 0, 0, 0, [
+                { offset: 0, color: '#B5CA19' },
+                { offset: 1, color: '#333' },
+              ]),
+            },
+          },
+        ],
+      },
+    };
+  },
+  components: {
+    Chart,
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+$box-height: 500px;
+#outbound {
+  width: 800px;
+  height: $box-height;
+  border-radius: 10px;
+}
+</style>
