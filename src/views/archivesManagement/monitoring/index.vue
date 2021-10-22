6f66ad0cf8353fa7e94e367547cb34e34d04c0c6
柏沐桜 <2585201479@qq.com>
2021-10-22 10:22:39 +0800

    优化 医院监测统计 打印、上传报告数 使用布尔值判断是否显示0

--- src/views/archivesManagement/monitoring/index.vue
+++ src/views/archivesManagement/monitoring/index.vue
@@ -101,18 +101,18 @@
 
       <el-table-column align="center" label="待打印报告数" prop="printNumber">
         <template slot-scope="scope">
-          <span :class="[scope.row.printNumber === 0 ? '' : 'skipStyle' ]" @click="skipHeart(scope.row, 'NOT_PRINT')">
+          <span :class="[ Boolean(scope.row.printNumber) === false ? '' : 'skipStyle' ]" @click="skipHeart(scope.row, 'NOT_PRINT')">
             {{
-              scope.row.printNumber
+              Boolean(scope.row.printNumber)  === false ? 0 : scope.row.printNumber
             }}
           </span>
         </template>
       </el-table-column>
       <el-table-column align="center" label="	待上传报告数" prop="uploadNumber">
         <template slot-scope="scope">
-          <span :class="[scope.row.uploadNumber === 0 ? '' : 'skipStyle' ]" @click="skipHeart(scope.row, 'uploadNumber')">
+          <span :class="[ Boolean(scope.row.uploadNumber) === false ? '' : 'skipStyle' ]" @click="skipHeart(scope.row, 'uploadNumber')">
             {{
-              scope.row.uploadNumber
+               Boolean(scope.row.uploadNumber) === false  ?  0 : scope.row.uploadNumber
             }}
           </span>
         </template>
