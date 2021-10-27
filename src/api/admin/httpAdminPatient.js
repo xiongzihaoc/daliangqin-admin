18510b70632a3dc0808742b97edb616f1400b2ee
柏沐桜 <2585201479@qq.com>
2021-10-27 17:41:28 +0800

    添加解锁接口

--- src/api/admin/httpAdminPatient.js
+++ src/api/admin/httpAdminPatient.js
@@ -32,6 +32,12 @@ export const httpAdminPatient = {
       method: 'put',
       data
     })
-  }
-
+  },
+  putUserUnLock(data) {
+    return request({
+      url: `user/unLock/${data.phone}`,
+      method: 'put',
+      data
+    })
+  },
 }
